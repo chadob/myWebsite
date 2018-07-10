3d7cdecd7d909c1637d99b09db9bcd750e665893
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2018-07-10 13:53:10 -0700

    still need to adjust modal looks

--- Scripts/homeController.js
+++ Scripts/homeController.js
@@ -16,12 +16,15 @@
   });
   $('.background-info-button').click(function () {
     console.log('hi')
+    $('.background-info').show();
     $('.background-info').css('opacity', '1');
   });
   $('.info-background').click(function () {
+    $('.background-info').hide();
     $('.background-info').css('opacity', '0');
   });
   $('.info-exit').click(function () {
+    $('.background-info').hide();
     $('.background-info').css('opacity', '0');
   });
   module.homeController = homeController;
